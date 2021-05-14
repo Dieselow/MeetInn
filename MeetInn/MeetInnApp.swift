7fd9a7bd42837630a5e6516f9da469c85176610b
Louis Dumont <louisdumontbarbosa@gmail.com>
2021-05-14 10:19:36 +0200

    Initial Commit

---
+++ MeetInn/MeetInnApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetInnApp.swift
+//  MeetInn
+//
+//  Created by Louis Dumont on 14/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct MeetInnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
